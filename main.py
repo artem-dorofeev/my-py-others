a74c8aad1d3a40454b563f21d1d8e71c96475668
Artem Dorofeev <a.dorofeev@ukr.net>
2023-05-19 22:30:44 +0300

    first

---
+++ main.py
@@ -0,0 +1,36 @@
+# программа тестирования
+# ticket_dict = {
+#     "quest" : "Choose the correct answer",
+#     "answer1" : "Artem",
+#     "quest2" : "Ira",
+#     "quest3" : "Diana",
+#     "quest4" : "Mila",
+# }
+
+# print(ticket_dict)
+# # print(ticket_dict["quest"])
+
+# for k in ticket_dict:
+#     print(ticket_dict[k])
+
+# tick_dic = {
+#     "quest" : "Choose the correct answer",
+#     1 : "jhgkejghek",
+#     2 : "aaaaaaaaa",
+#     3 : "bbbbbbb",
+# }
+
+# # print(tick_dic["quest"])
+
+# for k in tick_dic:
+#     print(tick_dic[k])
+
+t_dict = ('Question', 'Answer1', 'Answer2', 'Answer3', 'Answer4',)
+
+print(t_dict[0])
+print('Choose the correct answer')
+
+i = 1
+while i < 4:
+    print(t_dict[i])
+    i += 1
