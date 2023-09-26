58edc95da1ed01e0ccc4180a29376e3016b9e609
Artem Dorofeev <a.dorofeev@ukr.net>
2023-09-26 21:38:02 +0300

    leet5

---
+++ leetcode_05.py
@@ -0,0 +1,59 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        result = list1[::]
+        result.extend(list2)
+        res = sorted(result)
+        return res
+        # result = list1[::]
+        # # result.extend(list2)
+        # res = sorted(result.extend(list2))
+        # return res
+
+
+        
+  
+
+if __name__ == "__main__":
+    list1 = [1,2,4]
+    list2 = [1,3,4]
+    
+    test = Solution()
+    print(test.mergeTwoLists(list1, list2))
+    # s.romanToInt(test_str)
+
+"""
+21. Merge Two Sorted Lists
+
+You are given the heads of two sorted linked lists list1 and list2.
+
+Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+Return the head of the merged linked list.
+
+Input: list1 = [1,2,4], list2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+
+Example 2:
+
+Input: list1 = [], list2 = []
+Output: []
+
+Example 3:
+
+Input: list1 = [], list2 = [0]
+Output: [0]
+
+ 
+
+Constraints:
+
+    The number of nodes in both lists is in the range [0, 50].
+    -100 <= Node.val <= 100
+    Both list1 and list2 are sorted in non-decreasing order.
+
+"""
